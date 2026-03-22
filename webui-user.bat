6f6d2e9a5864f6ba1a7d45562bea04dfd11647e0
JungHo <iveinvalue@gmail.com>
2026-03-23 02:22:57 +0900

    init

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=py -3.10
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=venv
+set COMMANDLINE_ARGS=--xformers --medvram --opt-sdp-attention --no-half-vae
 
 call webui.bat
